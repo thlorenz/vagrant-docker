5b94889ecdb16633c30973902e6d5443e5f90bdf
Thorsten Lorenz <thlorenz@gmx.de>
2014-05-24 17:08:14 -0400

    upgrading to ubuntu trusty
    - takes forever to download
    - simplified bootstrap script (no more restart)
    - installing docker when bootstrapping instead of generating an install script

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,10 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "hashicorp/precise64"
+  config.vm.box = "vagrantbox_es/trusty64"
+  config.vm.box_url = 'https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box'
+  config.vm.host_name = 'trusty64-docker'
+
   config.vm.provision :shell, :path => "bootstrap.sh"
   for port in 49000..49900
     config.vm.network :forwarded_port, host: port, guest: port
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,18 @@
 
 apt-get update
 
-# make sure we get a good nodejs version
 apt-get install -y python-software-properties python g++ make
+
+# make sure we get a good nodejs version
 add-apt-repository ppa:chris-lea/node.js
+
 # for latest tmux and such
 add-apt-repository -y ppa:pi-rho/dev
+
+# docker
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+
 apt-get update
 apt-get install -y nodejs
 apt-get install -y tmux
@@ -61,16 +68,6 @@ cp -R ./dotfiles/fonts /usr/share/
 
 # docker
 
-## https://github.com/dotcloud/docker/issues/4568#issuecomment-37259489 
+## https://github.com/dotcloud/docker/issues/4568#issuecomment-37259489
 apt-get install -y cgroup-lite
-
-## upgrade linux kernel to support aufs
-apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
-
-## not sure if we could install docker before upgrading kernel, so we'll reboot first and do it after
-echo 'apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9' > ./install-docker.sh
-echo 'sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"'   >> ./install-docker.sh 
-echo 'apt-get update'                                                                                   >> ./install-docker.sh
-echo 'apt-get install -y lxc-docker'                                                                    >> ./install-docker.sh
-
-echo "rebooting to activate new linux kernel please run 'sudo sh ./install-docker.sh' after logging in again"
+apt-get install -y lxc-docker
